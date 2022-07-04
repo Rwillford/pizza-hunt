20f1e2533d984f28ba19049d77d249ef0368bc83
Bob <rwillford0719@gmail.com>
2022-07-04 15:07:05 -0500

    added replies

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -8,13 +8,29 @@ const commentController = {
         .then(({ _id }) => {
             return Pizza.findOneAndUpdate(
                 { _id: params.pizzaId },
-                { $push: { Comments: _id } },
+                { $push: { comments: _id } },
                 { new: true }
             );
         })
         .then(dbPizzaData => {
             if (!dbPizzaData) {
                 res.status(404).json({ message: 'No pizza was found wit htis id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No Pizza found with this id!'});
                 return;
             }
             res.json(dbPizzaData);
@@ -42,6 +58,16 @@ const commentController = {
             }
             res.sjson(dbPizzaData);
         })
+        .catch(err => res.json(err));
+    },
+
+    removeReply({ params }, res) {
+        Comment.findOneAndDelete(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.json(err));
     }
 };
